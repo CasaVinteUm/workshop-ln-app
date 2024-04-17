a49f64b88c88fcd08232da907e65749c0753010c
Lorenzo Maturano <maturanolorenzo@gmail.com>
2024-04-17 20:06:08 -0300

    im css king

--- client/src/routes/[winner]/+page.svelte
+++ client/src/routes/[winner]/+page.svelte
@@ -3,8 +3,6 @@
     import type { PageServerData } from "./$types";
 
     export let data: PageServerData;
-
-    let id: number | undefined = undefined;
     let payreq: string = "";
 
     function pay(): void {
@@ -13,8 +11,8 @@
         }
 
         payInvoice(payreq)
-            .then((payment) => {
-                id = payment.id;
+            .then((_) => {
+                alert("Payment successful");
             })
             .catch((e) => {
                 console.error("error paying invoice", e);
@@ -23,10 +21,14 @@
 </script>
 
 <div class="flex items-center justify-center min-h-screen">
-    <p class="h1">{data.winner} wins :P</p>
-    <label class="label">
-        <span>Invoice</span>
-        <input class="input" type="text" bind:value={payreq} />
-    </label>
-    <button on:click={pay}>Paga ae</button>
+    <div class="flex-row space-y-3">
+        <p class="h1">{data.winner} wins :P</p>
+        <label class="label">
+            <span>Invoice</span>
+            <input class="input" type="text" bind:value={payreq} />
+        </label>
+        <button on:click={pay} class="btn variant-filled-primary"
+            >Paga ae</button
+        >
+    </div>
 </div>
