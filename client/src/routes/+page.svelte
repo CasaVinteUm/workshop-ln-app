4dcbeb30691789456e2bca6c6422f8a88d1dbd6a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2024-04-17 18:59:01 -0300

    feat: fixing submit

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -56,8 +56,8 @@
         return;
     }
 
-    $: disabled = false;
-    // $: disabled = playerOneName === "" || playerTwoName === "" || !invoicesPaid;
+    // $: disabled = false;
+    $: disabled = playerOneName === "" || playerTwoName === "" || !invoicesPaid;
 </script>
 
 <svelte:head>
@@ -68,63 +68,57 @@
 </svelte:head>
 
 <div class="flex items-center justify-center min-h-screen">
-    <form on:submit|preventDefault={submit} class="pt-4">
-        <div class="flex space-x-10">
-            <div>
-                <label class="label">
-                    <span>Jogador 1</span>
-                    <input
-                        class="input"
-                        type="text"
-                        placeholder="Satoshi"
-                        bind:value={playerOneName}
+    <div class="flex space-x-10">
+        <div>
+            <label class="label">
+                <span>Jogador 1</span>
+                <input
+                    class="input"
+                    type="text"
+                    placeholder="Satoshi"
+                    bind:value={playerOneName}
+                />
+            </label>
+            {#if data.playerOne}
+                <button
+                    on:click={() =>
+                        data.playerOne &&
+                        copyToClipboard(data.playerOne.invoice.paymentRequest)}
+                >
+                    <qr-code
+                        class="visible max-h-96 max-w-96 dark:invert"
+                        contents={`lightning:${data.playerOne.invoice.paymentRequest}`}
                     />
-                </label>
-                {#if data.playerOne}
-                    <button
-                        on:click={() =>
-                            data.playerOne &&
-                            copyToClipboard(
-                                data.playerOne.invoice.paymentRequest,
-                            )}
-                    >
-                        <qr-code
-                            class="visible max-h-96 max-w-96 dark:invert"
-                            contents={`lightning:${data.playerOne.invoice.paymentRequest}`}
-                        />
-                    </button>
-                {/if}
-            </div>
-            <div>
-                <label class="label">
-                    <span>Jogador 2</span>
-                    <input
-                        class="input"
-                        type="text"
-                        placeholder="Nakamoto"
-                        bind:value={playerTwoName}
+                </button>
+            {/if}
+        </div>
+        <div>
+            <label class="label">
+                <span>Jogador 2</span>
+                <input
+                    class="input"
+                    type="text"
+                    placeholder="Nakamoto"
+                    bind:value={playerTwoName}
+                />
+            </label>
+            {#if data.playerTwo}
+                <button
+                    on:click={() =>
+                        data.playerTwo &&
+                        copyToClipboard(data.playerTwo.invoice.paymentRequest)}
+                >
+                    <qr-code
+                        class="visible max-h-96 max-w-96 dark:invert"
+                        contents={`lightning:${data.playerTwo.invoice.paymentRequest}`}
                     />
-                </label>
-                {#if data.playerTwo}
-                    <button
-                        on:click={() =>
-                            data.playerTwo &&
-                            copyToClipboard(
-                                data.playerTwo.invoice.paymentRequest,
-                            )}
-                    >
-                        <qr-code
-                            class="visible max-h-96 max-w-96 dark:invert"
-                            contents={`lightning:${data.playerTwo.invoice.paymentRequest}`}
-                        />
-                    </button>
-                {/if}
-            </div>
+                </button>
+            {/if}
         </div>
-        <div class="end-0 flex justify-end pt-4">
-            <button type="submit" class="variant-filled btn" {disabled}
-                >Lesgo</button
-            >
-        </div>
-    </form>
+    </div>
+    <div class="end-0 flex justify-end pt-4">
+        <button on:click={submit} class="variant-filled btn" {disabled}
+            >Lesgo</button
+        >
+    </div>
 </div>
