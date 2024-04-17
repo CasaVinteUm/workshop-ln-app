0ffd0ee7717e3be88b0b4bb2d126b4cde138943a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2024-04-17 20:51:42 -0300

    feat: checkpoint 1

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -2,4 +2,20 @@ import { z } from 'zod';
 
 const API_ENDPOINT: string = "http://localhost:8000";
 
-console.log("TODO");
+type GetInvoicesResponse = z.infer<typeof GetInvoicesResponse>;
+
+const GetInvoicesResponse = z.object({
+    playerOne: z.object({
+        id: z.number(),
+        invoice: z.string(),
+    }),
+    playerTwo: z.object({
+        id: z.number(),
+        invoice: z.string(),
+    }),
+});
+
+export const getInvoices = async (): Promise<GetInvoicesResponse> =>
+    fetch(`${API_ENDPOINT}/api/invoicesForPlayers`)
+        .then((a) => a.json())
+        .then(GetInvoicesResponse.parse);
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -9,7 +9,38 @@ const router = Router();
 const AMOUNT: number = 1000;
 
 router.get('/invoicesForPlayers', async (_req, res) => {
-    console.log("TODO");
+    try {
+        console.log('/invoicesForPlayers');
+
+        const invoiceForFirstPlayer = await createInvoice({ lnd, tokens: AMOUNT });
+        const playerOneInvoiceHash = invoiceForFirstPlayer.id;
+        const playerOnePaymentRequest = invoiceForFirstPlayer.request;
+
+        const invoiceForSecondPlayer = await createInvoice({ lnd, tokens: AMOUNT });
+        const playerTwoInvoiceHash = invoiceForSecondPlayer.id;
+        const playerTwoPaymentRequest = invoiceForSecondPlayer.request;
+
+        const count = invoicesDb.size;
+        const playerOneInvoiceId = count + 1;
+        const playerTwoInvoiceId = count + 2;
+
+        invoicesDb.set(playerOneInvoiceId, playerTwoInvoiceHash);
+        invoicesDb.set(playerTwoInvoiceId, playerOneInvoiceHash);
+
+        return res.status(200).send({
+            playerOne: {
+                id: playerOneInvoiceId,
+                invoice: playerOnePaymentRequest,
+            },
+            playerTwo: {
+                id: playerTwoInvoiceId,
+                invoice: playerTwoPaymentRequest,
+            }
+        });
+    } catch (err) {
+        console.error("invoicesForPlayers error: ", err);
+        return res.status(500).send({ error: "Server is dumb" });
+    }
 });
 
 router.get('/invoiceStatus/:id', async (req, res) => {
